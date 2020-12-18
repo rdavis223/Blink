4490bd9b74e880532d37c0830a347fb165ea1635
Maya Murad <mmurad7@uwo.ca>
2020-12-18 13:03:50 -0500

    Added player health system and damage script

---
+++ Blink/Assets/Scripts/Player Health/HealthManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar; 
+
+    // Temporary invincibility after damaged
+    public float invincibilityDuration;
+    private float invincibilityTimer;
+    private bool isInvincible;
+
+    // Game Over Stuff
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    private float fadeTimer;
+    public CanvasGroup gameOverScreen;
+    public CanvasGroup gameOverOptions;
+    private bool playerIsDead = false;
+    public float deathDelay = 2f;
+
+    // Damage Effect
+    public float damageEffectFadeSpeed; 
+    public float waitForDamageFade;
+    public Image damageScreenEffect;
+    private bool fadeIn;
+    private bool fadeOut;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth); // Set health bar
+        gameOverOptions.blocksRaycasts = false; 
+        gameOverScreen.alpha = 0;
+    }
+
+    void Update()
+    {
+        if (playerIsDead) 
+        {
+            Die();
+        }
+
+        if (fadeIn) // Fade in the damaged screen effect
+        {
+            damageScreenEffect.color = new Color(damageScreenEffect.color.r, damageScreenEffect.color.g, damageScreenEffect.color.b,
+                Mathf.MoveTowards(damageScreenEffect.color.a, 1f, damageEffectFadeSpeed * Time.deltaTime));
+
+            if (damageScreenEffect.color.a == 1f)
+            {
+                fadeIn = false;
+            }
+        }
+
+        if (fadeOut) // Fade out the damaged screen effect
+        {
+            damageScreenEffect.color = new Color(damageScreenEffect.color.r, damageScreenEffect.color.g, damageScreenEffect.color.b,
+                Mathf.MoveTowards(damageScreenEffect.color.a, 0f, damageEffectFadeSpeed * Time.deltaTime));
+
+            if (damageScreenEffect.color.a == 0f)
+            {
+                fadeOut = false;
+            }
+        }
+
+        if (isInvincible)
+        {
+            invincibilityTimer -= Time.deltaTime;
+
+            if (invincibilityTimer <= 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
+
+    public void HurtPlayer(int damage)
+    {
+        if (!isInvincible) // Only damage player if they're no longer invincible
+        {
+            currentHealth -= damage; // Decrease health
+            healthBar.SetHealth(currentHealth); // Set healthbar UI
+            invincibilityTimer = invincibilityDuration;
+            isInvincible = true; 
+
+            StartCoroutine(DamageEffectCoroutine());
+
+            if (currentHealth <= 0) // Player dies
+            {
+                StartCoroutine(DeathDelayCoroutine()); 
+            }
+        }
+    }
+
+    private IEnumerator DamageEffectCoroutine() // Show the damage effect
+    {
+        fadeIn = true;
+        yield return new WaitForSeconds(waitForDamageFade);
+        fadeIn = false;
+        fadeOut = true;
+    }
+
+    public void Die()
+    {
+        // Fade to game over screen
+        fadeTimer += Time.deltaTime;
+        gameOverScreen.alpha = fadeTimer / fadeDuration;
+
+        if (fadeTimer > fadeDuration + displayImageDuration)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            gameOverOptions.blocksRaycasts = true;
+            gameOverOptions.alpha = 1; // Show game over menu
+        }
+    }
+
+    // Delay the fade
+    private IEnumerator DeathDelayCoroutine()
+    {
+        yield return new WaitForSeconds(deathDelay);
+        playerIsDead = true;
+    }
+}

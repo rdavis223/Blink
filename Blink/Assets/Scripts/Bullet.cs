4032c7ab3c46db03fd94bc21addb498b87a5c999
Ryan Davis <rdavis54@uwo.ca>
2020-12-18 00:09:50 -0500

    Added bullet and assets

---
+++ Blink/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make the bullet move
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+    }
+}
---
+++ Blink/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    public Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.rotation = bulletPrefab.transform.rotation;
+            Debug.Log(bulletObject.transform.rotation.eulerAngles);
+            bulletObject.transform.localScale = new Vector3(50, 50, 50);
+            bulletObject.transform.position = playerCamera.transform.position + playerCamera.transform.forward;
+            bulletObject.transform.forward = playerCamera.transform.forward;
+            bulletObject.transform.rotation = Quaternion.LookRotation(playerCamera.transform.forward);
+            Debug.Log(bulletObject.transform.rotation.eulerAngles);
+
+
+        }
+    }
+}

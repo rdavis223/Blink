2ec8dfa043581f3f85606fa2d7c6ceecf81cb1f4
Maya Murad <mmurad7@uwo.ca>
2020-12-22 10:32:29 -0500

    Added main menu

---
+++ Blink/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Play
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+
+    // Quit
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

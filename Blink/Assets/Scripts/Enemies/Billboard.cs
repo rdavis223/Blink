7dfc105089c8f8dfa0fff3e0beffcbd97e0ebd16
Maya Murad <mmurad7@uwo.ca>
2021-03-15 14:51:20 -0400

    Health bar faces player
    The health bar now faces the player using the Billboard Script attached to the health bar slider.

---
+++ Blink/Assets/Scripts/Enemies/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Make healthbar face player
+public class Billboard : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+        Camera.main.transform.rotation * Vector3.up);
+    }
+}

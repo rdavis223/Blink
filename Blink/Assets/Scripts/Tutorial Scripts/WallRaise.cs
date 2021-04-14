02950f23100876a5a9cafaf6bcf32e70cb784e01
Maya Murad <mmurad7@uwo.ca>
2021-04-14 19:52:04 -0400

    tutorial level progress

---
+++ Blink/Assets/Scripts/Tutorial Scripts/WallRaise.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRaise : MonoBehaviour
+{
+    private bool raised;
+    public float speed;
+    public Transform target;
+
+    void Start()
+    {
+        raised = false;
+    }
+
+    void Update()
+    {
+        if (raised)
+        {
+            float step =  speed * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !raised)
+        {
+            raised = true;
+        }
+    }
+}

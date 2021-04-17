7fac32da195738ae11f7d5ed311f2f3cc33a369e
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-04-16 22:36:28 -0400

    fixed UI problem

--- Blink/Assets/Scripts/Player/PlayerShooting.cs
+++ Blink/Assets/Scripts/Player/PlayerShooting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
@@ -56,7 +56,13 @@ public class PlayerShooting : MonoBehaviour
 
 
     void updateAmmoText(string textToUpdate){
-        ammoText.GetComponent<TMP_Text>().text = textToUpdate;
+        if (SceneManager.GetActiveScene().name.Contains("Tutorial"))
+        {
+            ammoText.GetComponent<TMP_Text>().text = textToUpdate;
+        }else
+        {
+            ammoText.GetComponent<Text>().text = textToUpdate;
+        }
     }
 
     void reload(){

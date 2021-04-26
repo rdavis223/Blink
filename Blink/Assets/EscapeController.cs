cbc3e32109eedfd2f9694a078972376de516efcb
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-04-26 18:17:26 -0400

    Car sequence added with snapping, adding UI next

---
+++ Blink/Assets/EscapeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeController : MonoBehaviour
+{
+    public GameObject insideDoor;
+    public GameObject outsideDoor;
+
+    public GameObject car;
+
+    public bool objectiveRetrieved = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObjectiveCollected()
+    {
+        insideDoor.SetActive(false);
+        outsideDoor.SetActive(false);
+        objectiveRetrieved = true;
+        //displayUI here
+    }
+
+    public void StartCar()
+    {
+        car.SetActive(true);
+        BlinkMgr.Instance.BlinkTimer = 3.5f;
+    }
+}

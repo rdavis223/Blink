83cf3c14bfe5e37fee9c1f21737b054741d3a25d
rdavis223 <44733603+rdavis223@users.noreply.github.com>
2021-04-28 17:22:47 -0400

    added win mostly, adjusted volume

---
+++ Blink/Assets/GetawayCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetawayCar : MonoBehaviour
+{
+    private MovingPlatform mv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mv = this.GetComponent<MovingPlatform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z <= 10 && mv.attachedObject.name == "Player")
+        {
+            Debug.Log("Win");
+        }
+    }
+}
